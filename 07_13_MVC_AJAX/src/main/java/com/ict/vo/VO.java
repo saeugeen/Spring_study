aa33cb9830d200fde57959a083b91dd613eaa2d5
eugee <eugee@DESKTOP-Q4PM25B>
2021-07-13 16:45:50 +0900

    MVC_AJAX

---
+++ 07_13_MVC_AJAX/src/main/java/com/ict/vo/VO.java
@@ -0,0 +1,39 @@
+package com.ict.vo;
+
+public class VO {
+	private String idx, name, age, addr;
+
+	public String getIdx() {
+		return idx;
+	}
+
+	public void setIdx(String idx) {
+		this.idx = idx;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	public String getAddr() {
+		return addr;
+	}
+
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+	
+	
+}
